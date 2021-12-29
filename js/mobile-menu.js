4681b0b20feb2e4d654fcefe19845884a458eae5
Vitalina-Oleksiienko <vitalina_oleksiienko@yahoo.com>
2021-12-29 16:00:28 +0200

    add scss

---
+++ js/mobile-menu.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelector('.header__menu--mobile');
+let menu = document.querySelector('.header-menu--js');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
